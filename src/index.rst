15e307840f128e367f423b348219bc8eb109745c
Joni Nikki <72250615+j-nikki@users.noreply.github.com>
2022-10-11 16:34:00 +0300

    Add docs and GitHub workflow

---
+++ src/index.rst
@@ -0,0 +1,23 @@
+.. PostgreSQL for novices documentation master file, created by
+   sphinx-quickstart on Tue Oct 11 13:49:11 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PostgreSQL for novices's documentation!
+==================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: pg4n
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/pg4n/main.py
+++ src/pg4n/main.py
@@ -4,9 +4,10 @@ import sys
 import psqlparser
 import psqlwrapper
 
-if len(sys.argv) > 1:
-    wrap = psqlwrapper.PsqlWrapper(sys.argv[1].encode("utf-8"),
-                                   lambda x: "Helpful message",
-                                   psqlparser.PsqlParser())
-else:
-    print(f"{os.path.basename(sys.executable)} main.py <database name>")
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        wrap = psqlwrapper.PsqlWrapper(sys.argv[1].encode("utf-8"),
+                                       lambda x: "Helpful message",
+                                       psqlparser.PsqlParser())
+    else:
+        print(f"{os.path.basename(sys.executable)} main.py <database name>")
