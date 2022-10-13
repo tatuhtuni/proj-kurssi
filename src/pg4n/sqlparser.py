ceef48684b8d4abc98cddb86ac63e8ce0e6a5d04
Tatu Sirkeoja <tatu.sirkeoja@tuni.fi>
2022-10-13 19:39:06 +0300

    Refactors sqlparser into a class and adds tests

--- src/pg4n/sqlparser.py
+++ src/pg4n/sqlparser.py
@@ -1,37 +1,31 @@
 import sys
 import sqlglot
-from pprint import pprint
+from sqlglot.dialects.postgres import Postgres
 
+class SqlParser:
+    # Patches the postgres dialect to recognize bpchar
+    Postgres.Tokenizer.KEYWORDS["BPCHAR"] = sqlglot.TokenType.CHAR
 
-def parse(sql: str) -> sqlglot.exp.Expression:
-    return sqlglot.parse_one(sql)
+    def __init__(self):
+        self.dialect = "postgres"
 
+    def parse(self, sql: str) -> list[sqlglot.exp.Expression]:
+        """
+        Parses all the statements in 'sql'.
+        'sql' should be a string of one or more postgresql statements (delimited by ';').
+        The last ';' in 'sql' is optional.
+        Throws sqlglot.ParseError on invalid sql.
+        """
 
-USAGE = "usage: sql_parser.py"
+        return sqlglot.parse(sql, read=self.dialect)
 
+    def parse_one(self, sql: str) -> sqlglot.exp.Expression:
+        """
+        Parses the first statement in 'sql'.
+        Does not validate that 'sql' contains only 1 statement!
+        'sql' should be a postgresql statement.
+        The trailing ';' in 'sql' is optional.
+        Throws sqlglot.ParseError on invalid sql.
+        """
 
-def main():
-    if len(sys.argv) != 1:
-        print(USAGE, file=sys.stderr)
-        exit(1)
-
-    TABLE_NAME = "orders"
-    IMPOSSIBLE_STATEMENT = \
-        f"SELECT * FROM {TABLE_NAME} " \
-        "WHERE order_total_eur = 0 AND order_total_eur = 100;"
-
-    statement = IMPOSSIBLE_STATEMENT
-    sql_expression = parse(statement)
-    pprint(sql_expression)
-    print(80 * '=')
-
-    def get_where_expression(sql: str) -> str:
-        parsed = sqlglot.parse_one(sql)
-        where_statement = parsed.find(sqlglot.exp.Where)
-        return where_statement
-    where_expression = get_where_expression(statement)
-    pprint(where_expression)
-
-
-if __name__ == "__main__":
-    main()
+        return sqlglot.parse_one(sql, read=self.dialect)
