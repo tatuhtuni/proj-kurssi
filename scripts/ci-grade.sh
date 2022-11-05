e2e00553defa921f44823859e25c6af934188388
Joni Nikki <72250615+j-nikki@users.noreply.github.com>
2022-11-05 14:48:56 +0200

    Don't use pylint score

--- scripts/ci-grade.sh
+++ scripts/ci-grade.sh
@@ -8,8 +8,14 @@ $blackres
 \`\`\`
 </details>"
 
-lintres=$(pylint src --exit-zero)
-echo "<details><summary>pylint score: $(echo "$lintres" | tail -n 1 | grep -oP '(?<=at )\d+(?:\.\d+)?/\d+')</summary>
+lintres=$(pylint src --exit-zero -sn)
+lintscore=$(echo -e 'I\nR\nC\nW\nE\nF' |
+    xargs -I? bash -c 'echo "$(echo "$1" | grep -cP "^\S+\s+?")?"' -- "$lintres" |
+    grep -v '^0' | tr '\n' ' ' | xargs echo -n)
+if [[ "$lintscore" = "" ]]; then
+    lintscore="OK"
+fi
+echo "<details><summary>pylint score: $lintscore</summary>
 
 \`\`\`diff
 $(echo "$lintres" | sed -E '/\.py:[:0-9]+\s+[CE]/s/^/- /' | sed -E '/\.py:[:0-9]+\s+W/s/^/! /' | sed -E '/^([-!*])/!s/^/# /' | head -n -3)
