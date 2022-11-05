e08a1b24f8c32cf7ade690fd6549979ecd399b5c
Joni Nikki <72250615+j-nikki@users.noreply.github.com>
2022-11-05 14:57:21 +0200

    Recreate CI/CD PR comment

--- scripts/ci-grade.sh
+++ scripts/ci-grade.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+blackurl="[black](https://black.readthedocs.io/en/stable/usage_and_configuration/the_basics.html#diffs)"
+pylinturl="[pylint](https://pylint.pycqa.org/en/latest/user_guide/usage/output.html#source-code-analysis-section)"
+echo "ℹ Format info: $black • $pylinturl"
+
 blackres=$(black src --diff -q)
 echo "<details><summary>black diff: -$(echo "$blackres" | grep -cP '^-(?!-)') +$(echo "$blackres" | grep -cP '^\+(?!\+)')</summary>
 
@@ -18,6 +22,6 @@ fi
 echo "<details><summary>pylint score: $lintscore</summary>
 
 \`\`\`diff
-$(echo "$lintres" | sed -E '/\.py:[:0-9]+\s+[CE]/s/^/- /' | sed -E '/\.py:[:0-9]+\s+W/s/^/! /' | sed -E '/^([-!*])/!s/^/# /' | head -n -3)
+$(echo "$lintres" | sed -E '/\.py:[:0-9]+\s+[CEF]/s/^/- /' | sed -E '/\.py:[:0-9]+\s+W/s/^/! /' | sed -E '/^([-!*])/!s/^/# /' | head -n -3)
 \`\`\`
 </details>"
