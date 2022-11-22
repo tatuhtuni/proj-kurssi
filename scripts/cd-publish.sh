9974bab7212200596a92b636a4ae5cf337311c10
Joni Nikki <72250615+j-nikki@users.noreply.github.com>
2022-11-22 12:37:45 +0200

    Formatting

--- scripts/cd-publish.sh
+++ scripts/cd-publish.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+# base on:
 # https://github.com/python-poetry/poetry/issues/3670#issuecomment-776462445
 
-echo "Releasing for branch $GITHUB_REF"
 if [ "$GITHUB_REF" = "refs/heads/main" ]; then
   poetry version --short
 else
   poetry version $(poetry version --short)-dev.$GITHUB_RUN_NUMBER
   poetry version --short
   # or use --repository testpypi ?
+  # in case collisions etc. occur
+  # (requires a testpypi setup)
 fi
 echo -e "\nPublishing to version ref '$(poetry version --short)'...\n\n"
 poetry publish --no-interaction --build -u $PYPI_USER -p $PYPI_PASS
