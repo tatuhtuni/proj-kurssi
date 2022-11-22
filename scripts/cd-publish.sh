6febec34a0c614258383b4b018346eec8247bea2
Joni Nikki <72250615+j-nikki@users.noreply.github.com>
2022-11-22 13:00:21 +0200

    Try automatic versioning

--- scripts/cd-publish.sh
+++ scripts/cd-publish.sh
@@ -4,10 +4,9 @@
 # https://github.com/python-poetry/poetry/issues/3670#issuecomment-776462445
 
 if [ "$GITHUB_REF" = "refs/heads/main" ]; then
-  poetry version --short
+  poetry version $(poetry version --short).$GITHUB_RUN_NUMBER
 else
   poetry version $(poetry version --short)-dev.$GITHUB_RUN_NUMBER
-  poetry version --short
   # or use --repository testpypi ?
   # in case collisions etc. occur
   # (requires a testpypi setup)
