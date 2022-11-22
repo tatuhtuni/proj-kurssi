f1eba80b7fd925afe9929a83b99acb3abc8c28de
Joni Nikki <72250615+j-nikki@users.noreply.github.com>
2022-11-22 12:31:48 +0200

    Fix

--- scripts/cd-publish.sh
+++ scripts/cd-publish.sh
@@ -2,12 +2,13 @@
 
 # https://github.com/python-poetry/poetry/issues/3670#issuecomment-776462445
 
-echo "Branch name is $BRANCH_NAME"
-if [[ "$BRANCH_NAME" == *main ]]; then
+# If branch is main
+if [ "$GITHUB_REF" = "refs/heads/main" ]; then
   poetry version --short
 else
   poetry version $(poetry version --short)-dev.$GITHUB_RUN_NUMBER
   poetry version --short
+  # or use --repository testpypi ?
 fi
 echo -e "\nPublishing to version ref '$(poetry version --short)'...\n\n"
 poetry publish --no-interaction --build -u $PYPI_USER -p $PYPI_PASS
